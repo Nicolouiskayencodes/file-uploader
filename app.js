86566bbd256f5342950b7a1a152eac285f820b09
Nicolas Louis-Kayen <nicolouiskayencodes@gmail.com>
2024-11-13 20:13:33 -0800

    Add some basic styling

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const app = express();
 require('./config/passport')
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
